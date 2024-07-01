fb6f199ba036dd235b91518c5874c4f6d217abc8
abbasiiqra <abbasiiqra44@gmail.com>
2024-07-01 15:33:38 +0530

    updates

--- README.md
+++ README.md
@@ -1,7 +1,65 @@
-This code helps us identify malignant and benign tumors using Deep Learning .
-This also contains a functional website where user can input parameters and see the predicted result
+Breast Cancer Classification Website Using Neural Network
+Overview:
+This project involves the development of a web application that classifies breast cancer types using a neural network. The website allows users to input medical data and receive a prediction about whether the breast cancer is benign or malignant. This project leverages a neural network model trained on the Breast Cancer Wisconsin dataset.
 
-Description 
-Deep learning (also known as deep structured learning or hierarchical learning) is part of a broader family of machine learning methods based on learning data representations, as opposed to task-specific algorithms. Learning can be supervised, semi-supervised or unsupervised.
+Features:
+User-Friendly Interface: A simple and intuitive web interface for inputting medical data.
+Neural Network Prediction: Utilizes a trained neural network model to classify the breast cancer type.
+Real-Time Results: Instantaneous prediction results upon data submission.
+Data Visualization: Visualization of input features and prediction results for better understanding.
+Getting Started:
 
-Deep learning models are loosely related to information processing and communication patterns in a biological nervous system, such as neural coding that attempts to define a relationship between various stimuli and associated neuronal responses in the brain.
+1.Prerequisites
+Python 3.7.3
+Flask
+TensorFlow/Keras
+Scikit-learn
+Pandas
+NumPy
+
+Installation:
+
+Clone the Repository:
+git clone https://github.com/abbasiiqra/BreastCancerClassification.git
+cd BreastCancerClassification
+
+Create a Virtual Environment:
+python -m venv venv
+source venv/bin/activate   # On Windows use `venv\Scripts\activate`
+
+Install Dependencies:
+pip install -r requirements.txt
+
+Run the Application:
+flask run
+
+Access the Website:
+Open your web browser and go to http://127.0.0.1:5000/.
+
+Directory Structure:
+breast-cancer-classification/
+|--.venv
+|── static/
+|  |--images
+|── templates/
+|  |--index.html
+├── app.py
+└── model.pkl
+├── data/
+│  ├── data.csv
+├── models/
+│  ├── Breast_Cancer_Calssification_with_NN_Iqra.ipynb
+├── README.md
+├── requirements.txt
+└── .gitignore
+Usage
+Navigate to the Website:
+Enter http://127.0.0.1:5000/ in your web browser to access the web application.
+
+Input Medical Data:
+Fill in the required medical features (such as radius, texture, perimeter, area, smoothness, etc.).
+
+Get Prediction:
+Click the "Predict" button to receive the classification result (benign or malignant).
+
+
--- app.py
+++ app.py
@@ -1,70 +1,40 @@
-from flask import Flask, render_template, request
-import pickle
 import numpy as np
-
-model = pickle.load(open('classifier.pkl', 'rb'))
+import pandas as pd
+from flask import Flask, request, render_template
+import pickle
 
 app = Flask(__name__)
-
-
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
-def man():
-    return render_template('home.html')
-
-
-@app.route('/predict', methods=['POST'])
 def home():
-    data1 = request.form['a']
-    data2 = request.form['b']
-    data3 = request.form['c']
-    data4 = request.form['d']
-    data5 = request.form['e']
-    data6= request.form['f']
-    data7 = request.form['g']
-    data8 = request.form['h']
-    data9 = request.form['i']
-    data10= request.form['j']
-    data11= request.form['k']
-    data12= request.form['l']
-    data13 = request.form['m']
-    data14 = request.form['n']
-    data15= request.form['o']
-    data16= request.form['p']
-    data17 = request.form['q']
-    data18= request.form['r']
-    data19= request.form['s']
-    data20= request.form['t']
-    data21 = request.form['u']
-    data22 = request.form['v']
-    data23= request.form['w']
-    data24= request.form['x']
-    data25 = request.form['y']
-    data26= request.form['z']
-    data27= request.form['aa']
-    data28= request.form['bb']
-    data29 = request.form['cc']
-    data30= request.form['dd']
-  
-    arr = np.array([[data1, data2, data3, data4,data5, data6, data7, data8,data9, data10, data11, data12,data13, data14, data15, data16,data17, data18, data19, data20,data21, data22, data23, data24,data25,data26,data27,data28,data29,data30]])
-    pred = model.predict(arr)
-    return render_template('after.html', data=pred)
+    return render_template('index.html')
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    input_features = [float(x) for x in request.form.values()]
+    features_value = [np.array(input_features)]
+    
+    features_name = ['mean radius', 'mean texture', 'mean perimeter', 'mean area',
+       'mean smoothness', 'mean compactness', 'mean concavity',
+       'mean concave points', 'mean symmetry', 'mean fractal dimension',
+       'radius error', 'texture error', 'perimeter error', 'area error',
+       'smoothness error', 'compactness error', 'concavity error',
+       'concave points error', 'symmetry error', 'fractal dimension error',
+       'worst radius', 'worst texture', 'worst perimeter', 'worst area',
+       'worst smoothness', 'worst compactness', 'worst concavity',
+       'worst concave points', 'worst symmetry', 'worst fractal dimension']
+    
+    df = pd.DataFrame(features_value, columns=features_name)
+    output = model.predict(df)
+        
+    if output == 0:
+        res_val = "** breast cancer **"
+    else:
+        res_val = "no breast cancer"
+        
+
+    return render_template('index.html', prediction_text='Patient has {}'.format(res_val))
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    app.run()
